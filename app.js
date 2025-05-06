060bf54d72a84813e7ee66c59372c8c23cf78a29
Fabio Rostagno <fadarosta@gmail.com>
2025-05-06 09:50:51 -0300

    error handler

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const productsRouter = require('./routes/products');
-const cartsRouter    = require('./routes/carts');
+import express from 'express';
+import errorHandler from './middlewares/errorHandler.js';
 
+const app = express();
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
+app.use(errorHandler);
 app.use('/api/products', productsRouter);
-app.use('/api/carts',    cartsRouter);
+app.use('/api/carts', cartsRouter);
+
+app.listen(8080, () => {
+    console.log('Servidor escuchando en el puerto 8080');
+});
