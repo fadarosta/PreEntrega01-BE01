4556f137b8344390b645178c39d5962d742905a7
Fabio Rostagno <fadarosta@gmail.com>
2025-05-12 23:04:01 -0300

    ajustes en app

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 app.use(errorHandler);
